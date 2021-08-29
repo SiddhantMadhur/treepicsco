99a80e76cf5240805e3349c35a9f6e27cbc8c303
Siddhant Madhur <siddhant.madhur@gmail.com>
2021-08-30 00:39:21 +0400

    published-online

--- src/App.js
+++ src/App.js
@@ -4,7 +4,28 @@ import './App.css';
 function App() {
   return (
     <div>
-      
+      <div class='text-center' style={{
+        padding: '70px'
+      }}>
+        <h1>Tree Pics & Co</h1>
+      </div>
+      <br></br>
+      <div style={{
+        maxWidth: '800px',
+        margin: 'auto',
+        paddingLeft: 'auto',
+        paddingRight: 'auto'
+      }}>
+        <h3 class='text-center' >
+          Welcome to the official site for TreePicsCo
+        </h3>
+        <h4 class='text-center '>
+          The website is a work in progress. Please visit later.
+        </h4>
+      </div>
+      <div style={{
+        padding: '20px'
+      }} class='navbar fixed-bottom navbar-text'>Coded by Siddhant Madhur</div>
     </div>
   );
 }
